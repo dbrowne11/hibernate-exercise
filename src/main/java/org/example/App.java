ef7bcfe7bcf399e0659dd98b45149fbbed009756
Daniel Browne <danielsbrowne11@gmail.com>
2024-03-04 12:56:01 -0600

    refactored prompts into methods to improve readability

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,6 +14,47 @@ import org.hibernate.query.Query;
 
 public class App 
 {
+
+    static Employee promptAdd(Scanner scanner) {
+        System.out.println("database add selected");
+        System.out.println("Enter your name...");
+        String addName = scanner.next();
+        System.out.println("Enter your email...");
+        String addEmail = scanner.next();
+        // Create employee object -- this constructor increments primary key - id
+        return new Employee(addName, addEmail);
+    }
+
+    static Employee promptUpdate(Scanner scanner) {
+        // Prompt and collect input
+        System.out.println("Database Update Selected");
+        System.out.println("Enter target employee's ID");
+        int empId = scanner.nextInt();
+        System.out.println("Enter your name...");
+        String updateName = scanner.next();
+        System.out.println("Enter your email...");
+        String updateEmail = scanner.next();
+        // Create employee object
+        return new Employee(empId, updateName, updateEmail);
+    }
+
+    static Employee promptDelete(Scanner scanner) {
+        System.out.println("Database deletion selected");
+        System.out.println("Enter employee Id of employee");
+        int empId = scanner.nextInt();
+        // Set up for deletion
+        Employee toDelete = new Employee();
+        toDelete.setId(empId);
+        return toDelete;
+    }
+
+    // Returns int Id
+    static int promptGet(Scanner scanner) {
+        System.out.println("Selected get employee");
+        System.out.println("Enter employee ID");
+        int empId = scanner.nextInt();
+        return empId;
+    }
     public static void main( String[] args )
     {
         // Hibernate setup
@@ -23,7 +64,6 @@ public class App
         SessionFactory sessionFactory = config.buildSessionFactory();
         // Open the session
         Session session = sessionFactory.openSession();
-        //Transaction transaction = session.beginTransaction();
 
         // Setup loop and input scanner
         boolean flag = true;
@@ -42,36 +82,18 @@ public class App
                     6: Exit""");
 
             int input = scanner.nextInt();
-            //Variables common between multiple switch statements
-            int empId;
-            // Handle operations
+
             switch (input) {
                 // Adding to databse
                 case 1:
-                    // Prompting and collecting input
-                    System.out.println("database add selected");
-                    System.out.println("Enter your name...");
-                    String addName = scanner.next();
-                    System.out.println("Enter your email...");
-                    String addEmail = scanner.next();
-                    // Create employee object -- this constructor increments primary key - id
-                    Employee toAdd = new Employee(addName, addEmail);
+                    Employee toAdd = promptAdd(scanner);
                     // add to db
                     session.save(toAdd);        // save is depreciated, using persist I believe is now recommended
                     System.out.println("Inserted: " + toAdd);
                     break;
                 // Updating database
                 case 2:
-                    // Prompy and collect input
-                    System.out.println("Database Update Selected");
-                    System.out.println("Enter target employee's ID");
-                    empId = scanner.nextInt();
-                    System.out.println("Enter your name...");
-                    String updateName = scanner.next();
-                    System.out.println("Enter your email...");
-                    String updateEmail = scanner.next();
-                    // Create employee object
-                    Employee toUpdate = new Employee(empId, updateName, updateEmail);
+                    Employee toUpdate = promptUpdate(scanner);
                     // execute the update
                     try {
                         session.update(toUpdate);   // update depreciated -- merge is now preferred
@@ -84,12 +106,7 @@ public class App
                 //Delete from database
                 case 3:
                     // Prompt and read input
-                    System.out.println("Database deletion selected");
-                    System.out.println("Enter employee Id of employee");
-                    empId = scanner.nextInt();
-                    // Set up for deletion
-                    Employee toDelete = new Employee();
-                    toDelete.setId(empId);
+                    Employee toDelete = promptDelete(scanner);
                     // Execute deletion -- error occurs on not found
                     try {
                         session.delete(toDelete);
@@ -97,15 +114,12 @@ public class App
                         System.out.println("Deletion failed with error message: " + e.getMessage());
                     }
 
-                    System.out.println("Deleted Employee with Id: " + empId);
+                    System.out.println("Deleted Employee with Id: " + toDelete.getId());
                     break;
 
                 // Get by ID
                 case 4:
-                    // Prompt and read input
-                    System.out.println("Selected get employee");
-                    System.out.println("Enter employee ID");
-                    empId = scanner.nextInt();
+                    int empId = promptGet(scanner);
                     // Execute -- error occurs if id not found
                     try {
                         Employee toGet = session.load(Employee.class, empId);
@@ -114,7 +128,6 @@ public class App
                     catch (Exception e) {
                         System.out.println("Update failed with error message: " + e.getMessage());
                     }
-
                     break;
                 // Get all Employees
                 case 5:
@@ -127,8 +140,9 @@ public class App
                         System.out.println(employee.toString());
                     }
                     break;
+                // Exit
                 case 6:
-                    // Exit
+
                     flag = false;
                     System.out.println("Exiting...");
                     break;
